d34ad1787361167052de5aa66aaf81ce5e722240
Aditya magadum <adityamagadum108@gmail.com>
2026-07-08 13:38:19 +0530

    airthmatic operations using script

---
+++ airthmatic_op.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo 
+read -p "Enter your First number: " num1
+echo 
+read -p "Enter second Number : " num2
+echo
+echo "num1 = $num1 num2= $num2"
+
+echo 
+echo "Additon of $num1 and $num2 = $(($num1+$num2))"
+
+echo 
+echo "substraction of $num1 and $num2 = $(($num1-$num2))"
+echo 
+echo "Multiplication of $num1 and $num2 = $(($num1*$num2))"
+echo 
+echo "division of $num1 and $num2 = $(($num1/$num2))"
