d45ac6f6c15fbeae59cce565e2a29e6effc8384e
Aditya magadum <adityamagadum108@gmail.com>
2026-07-08 13:48:36 +0530

    if else scripting

---
+++ if.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "Enter Your marks : " marks
+echo "wait we are evaluating your Query!!"
+echo 
+echo
+if [[ $marks -gt 40 ]]
+then
+  echo "Congratulations!! You are Passed :)"
+else
+  echo "Dont Loose hopes Try agin You can do it!"
+fi
+echo 
+echo
